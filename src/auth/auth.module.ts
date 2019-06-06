f4e48aff6f8a134dedc8d08c0c1af445d9c1ec61
albert5287 <albertgrac@gmail.com>
2019-06-06 19:05:26 +0200

    create and use the auth service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -38,7 +38,7 @@ export const firebaseConfig : FirebaseAppConfig = {
     AngularFireModule.initializeApp(firebaseConfig),
     AngularFireAuthModule,
     AngularFireDatabaseModule,
-    SharedModule
+    SharedModule.forRoot()
   ]
 })
 export class AuthModule {}
