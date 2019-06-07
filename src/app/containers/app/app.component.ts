b07cb43a43d86b8b4a2e5c2d62f1efbcd9f9dca5
albert5287 <albertgrac@gmail.com>
2019-06-07 11:54:22 +0200

    trying to set up the reactive state

--- src/app/containers/app/app.component.ts
+++ src/app/containers/app/app.component.ts
@@ -1,17 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import { Store } from '../../../store';
+import { AuthService, User } from '../../../auth/shared/services/auth/auth.service';
 
 @Component({
   selector: 'my-app',
   styleUrls: [ './app.component.css' ],
   template: `
     <div>
-      Hello !!!
+      <h1>{{ user$ | async | json }} </h1>
       <div class="wrapper">
         <router-outlet></router-outlet>
       </div>
     </div>
   `,
 })
-export class AppComponent  {
-  name = 'Angular';
+export class AppComponent implements OnInit, OnDestroy {
+
+  user$: Observable<User>;
+  subscription: Subscription;
+
+  constructor(
+    private store: Store,
+    private authService: AuthService,
+  ){}
+
+  ngOnInit(){
+    console.log('hello', AuthService);
+    //this.subscription = this.authService.auth$.subscribe();
+    this.user$ = this.store.select<User>('User');
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
+  }
+  
 }
