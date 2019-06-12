353536fe48772d2928dbdf43012806b90c3f274b
Albert Gracia <Albert.Gracia@avaloq.com>
2019-06-12 14:46:46 +0200

    guard the routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,7 +18,9 @@ import { AppNavComponent } from './components/app-nav/app-nav.component';
 
 
 // routes
-export const ROUTES: Routes = [];
+export const ROUTES: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'schedule' }
+];
 
 @NgModule({
   imports:      [ 
