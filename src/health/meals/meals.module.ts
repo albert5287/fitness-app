ec78e837262432e290e88225873f167621806816
Albert Gracia <Albert.Gracia@avaloq.com>
2019-06-13 17:06:23 +0200

    -improve meals component -create the meal component

--- src/health/meals/meals.module.ts
+++ src/health/meals/meals.module.ts
@@ -6,10 +6,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 //containers
 import { MealsComponent } from './containers/meals/meals.component';
+import { MealComponent } from './containers/meal/meal.component';
 import { SharedModule } from '../shared/shared.module';
 
 export const ROUTES: Routes = [
-    {path: '', component: MealsComponent}
+    {path: '', component: MealsComponent},
+    {path: 'new', component: MealComponent}
 ]
 
 @NgModule({
@@ -20,7 +22,8 @@ export const ROUTES: Routes = [
         SharedModule
     ],
     declarations: [
-        MealsComponent
+        MealsComponent,
+        MealComponent
     ]
 })
 export class MealsModule {}
