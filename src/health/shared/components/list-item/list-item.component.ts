4c67ccb37d8e12458d67a78c6ba26f50b6090c37
Albert Gracia <Albert.Gracia@avaloq.com>
2019-06-14 16:28:43 +0200

    -some work in the list-itemcomponent

--- src/health/shared/components/list-item/list-item.component.ts
+++ src/health/shared/components/list-item/list-item.component.ts
@@ -1,17 +1,64 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
     selector: 'list-item',
+    changeDetection: ChangeDetectionStrategy.OnPush,
     styleUrls: ['list-item.component.scss'],
     template: `
         <div class="list-item">
-            {{item | json}}
+           <a [routerLink]="getRoute(item)">
+                <p class="list-item__name">{{ item.name }}</p>
+                <p class="list-item__ingredients">
+                    <span>
+                        {{ item.ingredients }}
+                    </span>
+                </p>
+           </a>
+
+           <div class="list-item__delete"
+            *ngIf="toggled">
+                <p>Delete item?</p>
+                <button
+                    class="confirm"
+                    type="button"
+                    (click)="removeItem()">
+                        Yes
+                </button>
+                <button
+                    class="cancel"
+                    type="button"
+                    (click)="toogle()">
+                        No
+                </button>
+           </div>
+
+           <button
+            class="trash"
+            type="button"
+            (click)="toggle()">
+                <img src="assets/images/remove.svg" />
+           </button>
         </div>
     `
 })
 export class ListItemComponent {
 
+    toggled = false;
+
     @Input()
     item: any;
+
     constructor() {}
+
+    getRoute(item: any){
+        return [`../meals`, item.$key];
+    }
+
+    toggle(){
+        this.toggled = !this.toggled;
+    }
+
+    removeItem(){
+        
+    }
 }
